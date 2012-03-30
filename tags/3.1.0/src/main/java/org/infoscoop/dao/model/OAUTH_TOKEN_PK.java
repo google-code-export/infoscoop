be6b7e332a2434d63f0d3e70334fc7d4472ab178
ak.nishiumi@gmail.com <ak.nishiumi@gmail.com@d4e23b88-c782-11de-b6dc-25c59a28ecba>
2012-03-30 10:56:28 +0000

    infoScoop OpenSource 3.1.0 official release

---
+++ tags/3.1.0/src/main/java/org/infoscoop/dao/model/OAUTH_TOKEN_PK.java
@@ -0,0 +1,22 @@
+package org.infoscoop.dao.model;
+
+import org.infoscoop.dao.model.base.BaseOAUTH_TOKEN_PK;
+
+public class OAUTH_TOKEN_PK extends BaseOAUTH_TOKEN_PK {
+	private static final long serialVersionUID = 1L;
+
+/*[CONSTRUCTOR MARKER BEGIN]*/
+	public OAUTH_TOKEN_PK () {}
+	
+	public OAUTH_TOKEN_PK (
+		java.lang.String uid,
+		java.lang.String fkOAuthId) {
+
+		super (
+			uid,
+			fkOAuthId);
+	}
+/*[CONSTRUCTOR MARKER END]*/
+
+
+}
